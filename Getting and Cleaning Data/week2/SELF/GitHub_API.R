35df194577b0e180d70639f5298edf27d3dd6877
ravi-prakash1907 <1907prakash.ravi@gmail.com>
2020-08-22 21:14:04 +0530

    GitHub API script (blank init)

---
+++ Getting and Cleaning Data/week2/SELF/GitHub_API.R
@@ -0,0 +1,13 @@
+# https://rpubs.com/ninjazzle/DS-JHU-3-2-Q2
+
+## Sol to que1  ===   wek2 quiz
+
+
+
+
+#########################
+
+## Q4:
+# How many characters are in the 10th, 20th, 30th and 100th lines of HTML from this page:
+# http://biostat.jhsph.edu/~jleek/contact.html
+# (Hint: the nchar() function in R may be helpful)
